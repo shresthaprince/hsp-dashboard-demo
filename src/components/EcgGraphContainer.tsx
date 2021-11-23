c40f500ae5545a2ef2316c481cd8244bb6d83ecc
Rico-sudo <albericosss@hotmail.it>
2021-11-23 23:14:09 +1000

    added rechart library and showed the data for the first time

--- src/components/EcgGraphContainer.tsx
+++ src/components/EcgGraphContainer.tsx
@@ -1,4 +1,5 @@
 import { gql, useQuery } from '@apollo/client';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
 const GET_ECGS = gql`
 query getEcg {
@@ -18,11 +19,34 @@ const EcgGraphContainer = () => {
     if (error) return <div>{`Error! ${error.message}`}</div>;
 
     return (
-        <div className="h-100 bg-dark">{data.listEcgs.items.map((e: any) => <div>
-            <span className="text-light">{`Sensor Id: ${e.sensorId}`}</span>
-            <span className="text-light">{` Ecg: ${e.ecg}`}</span></div>)}
+        <div className="h-100 bg-dark">
+              <ResponsiveContainer width="90%" aspect={3}>
+    <LineChart
+      width={500}
+      height={300}
+      data={data.listEcgs.items}
+      margin={{
+        top: 15,
+        right: 30,
+        left: 20,
+        bottom: 5,
+      }}
+    >
+      <CartesianGrid vertical={false}  stroke="#243240"/>
+      <XAxis dataKey="sensorId" tick={{fill:"#fff"}}/>
+      <YAxis tick={{fill:"#fff"}} />
+      <Tooltip contentStyle={{backgroundColor:"#8884d8",color:"#fff"}} itemStyle={{color:"#fff"}} cursor={false} />
+      {/* legend will be usef when we will have multiple sensors */}
+      {/* <Legend />  */}
+      {/* <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} /> */}
+      <Line type="monotone" dataKey="ecg" stroke="#8884d8" strokeWidth="2" dot={{fill:"2e4355" ,stroke:"#8884d8" ,strokeWidth:2 ,r:5}} activeDot={{ r: 8 }} />
+    </LineChart>
+  </ResponsiveContainer>
+  
         </div>
     );
 }
 
 export default EcgGraphContainer;
+
+
