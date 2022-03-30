6c1bfaa13b86b4908e8b0296181e12fa51c7f458
Rico-sudo <albericosss@hotmail.it>
2022-03-30 14:56:43 +1100

    Buttons card components for the user selection

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
-import "./App.css";
+// Components
+import Card from './Card';
+// Styles
+import { Wrapper } from './App.styles';
 
-function App() {
+
+import logo1 from './img/l2.png';
+
+import videou from './video/u1.mp4';
+
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <h1>Health Sensor Platform</h1>
-    </div>
+    <Wrapper>
+      <Card logo={logo1} video={videou} />
+      <Card logo={logo1} video={videou} />
+
+    </Wrapper>
   );
 }
 
---
+++ src/Card.tsx
@@ -0,0 +1,36 @@
+// Styles
+import { Wrapper } from './Card.styles';
+
+type Props = {
+  logo: string;
+  video: string;
+};
+
+const Card: React.FC<Props> = ({ logo, video }) => {
+  const handleOnMouseOver = (e: React.MouseEvent<HTMLVideoElement>) => {
+    e.currentTarget.play();
+  };
+
+  const handleOnMouseOut = (e: React.MouseEvent<HTMLVideoElement>) => {
+    e.currentTarget.pause();
+  };
+
+  return (
+    <Wrapper>
+      <div className="border" />
+      <img src={logo} alt='logo' />
+      <p id='user'>User A</p>
+      <video
+        loop
+        preload='none'
+        muted // Needs to be there to be able to play
+        onMouseOver={handleOnMouseOver}
+        onMouseOut={handleOnMouseOut}
+      >
+        <source src={video} type='video/mp4' />
+      </video>
+    </Wrapper>
+  );
+};
+
+export default Card;
