91319930357de8f599e5de12840184bb73164260
Prince <shresthaprince@hotmail.com>
2022-04-04 19:01:18 +1000

    Updated dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
-import { ApolloProvider } from '@apollo/client';
-import EcgGraph from './components/EcgGraph';
-import client from './config/ApolloClient';
+import { ApolloProvider } from "@apollo/client";
+import EcgGraph from "./components/EcgGraph";
+import client from "./config/ApolloClient";
 
 const App = () => {
   return (
     <ApolloProvider client={client}>
-      <EcgGraph />
+      <div className="bg-black h-100 p-5">
+        <div className="pt-3">
+          <h1 className="ff-orbitron text-white ms-3 mb-3">
+            Heart Rate Monitor
+          </h1>
+          <EcgGraph />
+        </div>
+      </div>
     </ApolloProvider>
-  )
-}
-
+  );
+};
 
 export default App;
--- src/config/AppSync.ts
+++ src/config/AppSync.ts
@@ -1,8 +1,10 @@
 const AppSyncConfig = {
-    "ApiUrl": "https://4riomytsjvdnzft2o3iv3p4c4m.appsync-api.ap-southeast-2.amazonaws.com/graphql",
-    "ApiSubscriptionUrl": "https://4riomytsjvdnzft2o3iv3p4c4m.appsync-api.ap-southeast-2.amazonaws.com/subscriptions",
-    "Region": "ap-southeast-2",
-    "AuthMode": "AMAZON_COGNITO_USER_POOLS",
-}
+  ApiUrl:
+    "https://moopkgmu3vfoxlfxofrzovbo2u.appsync-api.ap-southeast-2.amazonaws.com/graphql",
+  ApiSubscriptionUrl:
+    "https://moopkgmu3vfoxlfxofrzovbo2u.appsync-api.ap-southeast-2.amazonaws.com/subscriptions",
+  Region: "ap-southeast-2",
+  AuthMode: "AMAZON_COGNITO_USER_POOLS",
+};
 
-export default AppSyncConfig
+export default AppSyncConfig;
