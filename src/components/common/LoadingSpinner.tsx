1eda7f07f5cbbb19064c57ec64a8bf1366e14b34
Rico-sudo <albericosss@hotmail.it>
2022-04-10 06:11:16 +1000

    code for the mega board for the gps and temp&hum setup

--- src/components/common/LoadingSpinner.tsx
+++
@@ -1,8 +0,0 @@
-const LoadingSpinner = () => (
-  <div
-    style={{ height: "1.5rem", width: "1.5rem" }}
-    className="spinner-border"
-  />
-);
-
-export default LoadingSpinner;
--- src/config/theme.ts
+++
@@ -1,7 +0,0 @@
-const theme = {
-  red: "primary",
-  black: "black",
-  white: "white",
-};
-
-export default theme;
